1c3492282391fb4a4c7c2b7e4518eff1d4457202
SrajanAgrawal <srajangarg8273@gmail.com>
2023-10-29 08:41:45 +0530

    fourth commit

--- components/UpcomingJobs.tsx
+++ components/UpcomingJobs.tsx
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
-import Slider from "react-slick";
+import JobCard from "./JobCard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import JobCard from "./JobCard"; // Import the JobCard component
 
 // Sample job data
 const jobs = [
     { title: "Job 1", description: "Description for Job 1" },
     { title: "Job 2", description: "Description for Job 2" },
     { title: "Job 3", description: "Description for Job 3" },
-    { title: "Job 1", description: "Description for Job 1" },
-    { title: "Job 2", description: "Description for Job 2" },
-    { title: "Job 3", description: "Description for Job 3" },
+    { title: "Job 4", description: "Description for Job 4" },
+    { title: "Job 5", description: "Description for Job 5" },
+    { title: "Job 6", description: "Description for Job 6" },
     // Add more job data
 ];
 
 function UpcomingJobs() {
     const [searchTerm, setSearchTerm] = useState("");
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const cardsPerPage = 3;
 
     const filteredJobs = jobs.filter((job) =>
         job.title.toLowerCase().includes(searchTerm.toLowerCase())
@@ -24,45 +25,26 @@ function UpcomingJobs() {
 
     const handleSearch = (e: { target: { value: React.SetStateAction<string>; }; }) => {
         setSearchTerm(e.target.value);
+        setCurrentIndex(0); // Reset to the first page when searching
     };
 
-    const NextArrow = (props: React.JSX.IntrinsicAttributes & React.ClassAttributes<HTMLButtonElement> & React.ButtonHTMLAttributes<HTMLButtonElement>) => (
-        <button {...props} className="slick-next">
-            <FontAwesomeIcon icon={faArrowRight} />
-        </button>
-    );
-
-    const PrevArrow = (props: React.JSX.IntrinsicAttributes & React.ClassAttributes<HTMLButtonElement> & React.ButtonHTMLAttributes<HTMLButtonElement>) => (
-        <button {...props} className="slick-prev">
-            <FontAwesomeIcon icon={faArrowLeft} />
-        </button>
-    );
-
-    const sliderSettings = {
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                },
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                },
-            },
-        ],
-        prevArrow: <PrevArrow />,
-        nextArrow: <NextArrow />,
+    const handleNext = () => {
+        if (currentIndex < filteredJobs.length - cardsPerPage) {
+            setCurrentIndex(currentIndex + 1);
+        }
     };
+
+    const handlePrev = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex(currentIndex - 1);
+        }
+    };
+
+    const visibleJobs = filteredJobs.slice(currentIndex, currentIndex + cardsPerPage);
 
     return (
-        <div className="container mx-auto">
-            <div className="container mx-auto p-4 bg-black text-white flex flex-col">
+        <div className="bg-black min-h-screen py-8 text-white">
+            <div className="container mx-auto">
                 <h1 className="text-2xl font-semibold mb-4 text-center">
                     Upcoming Jobs Near You
                 </h1>
@@ -76,17 +58,36 @@ function UpcomingJobs() {
                         className="w-64 border border-gray-300 rounded-lg p-2"
                     />
                 </div>
-            </div>
 
-            <div className="container mx-auto p-4 bg-white text-black flex overflow-hidden">
-                <Slider {...sliderSettings} className="mb-4">
-                    {filteredJobs.map((job, index) => (
-                        <JobCard key={index} title={job.title} description={job.description} />
-                    ))}
-                </Slider>
-            </div>
+                <div className="relative">
+                    <div className="flex justify-between items-center">
+                        <button
+                            className={`slider-button prev ${currentIndex === 0 ? 'opacity-50 cursor-not-allowed' : ''}`}
+                            onClick={handlePrev}
+                            disabled={currentIndex === 0}
+                        >
+                            <FontAwesomeIcon icon={faArrowLeft} />
+                        </button>
 
-        </div>
+                        <div className="flex flex-wrap slider">
+                            {visibleJobs.map((job, index) => (
+                                <div key={index} className="slider-item w-full md:w-1/2 lg:w-1/3 p-4">
+                                    <JobCard title={job.title} description={job.description} />
+                                </div>
+                            ))}
+                        </div>
+                        <button
+                            className={`slider - button next ${currentIndex >= filteredJobs.length - cardsPerPage ? 'opacity-50 cursor-not-allowed' : ''}`}
+                            onClick={handleNext}
+                            disabled={currentIndex >= filteredJobs.length - cardsPerPage}
+                        >
+                            <FontAwesomeIcon icon={faArrowRight} />
+                        </button>
+                    </div>
+
+                </div>
+            </div >
+        </div >
     );
 }
 
